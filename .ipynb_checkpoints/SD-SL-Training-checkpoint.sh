93d7b2860bc8e233d8745603c52787f68b0e2086
981526092 <wu981526092@163.com>
2023-06-29 14:22:05 +0000

    add update

---
+++ .ipynb_checkpoints/SD-SL-Training-checkpoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Declare variables
+MODEL_PATH='distilbert-base-uncased'
+BIAS_TYPE='religion'
+DATASET_SELECT='intersentence intrasentence crowspairs'
+BATCH_SIZE=16
+EPOCH=6
+LEARNING_RATE=2e-5
+PYTHON_SCRIPT_PATH='../bias_detector/training/trainer_SD_SL.py' # Replace with the path to your Python script
+
+# Execute the Python script with the command-line arguments
+python3 $PYTHON_SCRIPT_PATH --model_path $MODEL_PATH --bias_type $BIAS_TYPE --dataset_select $DATASET_SELECT --batch_size $BATCH_SIZE --epoch $EPOCH --learning_rate $LEARNING_RATE
--- SD-TL-Training.sh
+++ SD-TL-Training.sh
@@ -7,7 +7,7 @@ DATASET_SELECT='intersentence'
 BATCH_SIZE=16
 EPOCH=6
 LEARNING_RATE=2e-5
-PYTHON_SCRIPT_PATH='/Users/zekunwu/Desktop/bias_detector/training/trainer_SD_TL.py' # Replace with the path to your Python script
+PYTHON_SCRIPT_PATH='../bias_detector/training/trainer_SD_TL.py' # Replace with the path to your Python script
 
 # Execute the Python script with the command-line arguments
 python3 $PYTHON_SCRIPT_PATH --model_path $MODEL_PATH --bias_type $BIAS_TYPE --dataset_select $DATASET_SELECT --batch_size $BATCH_SIZE --epoch $EPOCH --learning_rate $LEARNING_RATE
